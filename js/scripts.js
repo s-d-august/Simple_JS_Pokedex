aa82a658169846f0d0ac3ad2810650f358bab2b1
s-d-august <sinclairaugust@gmail.com>
2024-01-13 12:30:20 -0600

    added gardevoir to pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
 let pokemonList = [
-    { name: 'Mimikyu', type: ['ghost', 'fairy'], number: '#0778', weight: 1.5}
+    { name: 'Mimikyu', type: ['ghost', 'fairy'], number: '#0778', weight: 1.5},
+    { name: 'Gardevoir', type: ['psychic', 'fairy'], number: '#0282', weight: 106.7}
 ];
