e312605bc96bc1f59ef766758271804999da232e
s-d-august <sinclairaugust@gmail.com>
2024-01-13 12:33:19 -0600

    added polteageist to pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 let pokemonList = [
     { name: 'Mimikyu', type: ['ghost', 'fairy'], number: '#0778', weight: 1.5},
-    { name: 'Gardevoir', type: ['psychic', 'fairy'], number: '#0282', weight: 106.7}
+    { name: 'Gardevoir', type: ['psychic', 'fairy'], number: '#0282', weight: 106.7},
+    { name: 'Polteageist', type: 'ghost', number: '#08550', weight: 0.9},
 ];
