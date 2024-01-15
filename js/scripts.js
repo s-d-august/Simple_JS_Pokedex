8ba325b5ff60cd55951585c0ee36e42b0faa6b10
s-d-august <sinclairaugust@gmail.com>
2024-01-15 15:49:52 -0600

    changed "type" to "types"
    also made polteageist's type an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let pokemonList = [
-    { name: 'Mimikyu', type: ['ghost', 'fairy'], number: '#0778', weight: 1.5},
-    { name: 'Gardevoir', type: ['psychic', 'fairy'], number: '#0282', weight: 106.7},
-    { name: 'Polteageist', type: 'ghost', number: '#08550', weight: 0.9},
+    { name: 'Gardevoir', types: ['psychic', 'fairy'], number: '#0282', weight: 106.7},
+    { name: 'Polteageist', types: ['ghost'], number: '#08550', weight: 0.9},
 ];
+
+
