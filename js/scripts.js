66d23c1bc29a341738b88c260cf2d7eca9ca19a5
s-d-august <sinclairaugust@gmail.com>
2024-01-15 16:00:27 -0600

    added if...else to flag big pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,14 @@ let pokemonList = [
     { name: 'Polteageist', types: ['ghost'], number: '#08550', weight: 0.9},
 ];
 
-
+// Lists names and weights of pokemonList
 for (let i = 0; i < pokemonList.length; i++){
     let pokename = pokemonList[i].name;
     let pokeweight = pokemonList[i].weight;
-    document.write(`${pokename} (weight: ${pokeweight})<br>`)
-
-
+    if (pokeweight > 100) {
+        document.write(`${pokename} (weight: ${pokeweight}) Wow, that's big!<br>`)
+    } // Pokemon with weight over 100 get flagged with "Wow, that's big!"
+    else{
+        document.write(`${pokename} (weight: ${pokeweight})<br>`)}
+    
 }
