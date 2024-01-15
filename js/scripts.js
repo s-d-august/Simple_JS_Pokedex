df037f63d65f02b840bcc9bfa3df3d13523229dd
s-d-august <sinclairaugust@gmail.com>
2024-01-15 15:50:24 -0600

    added loop to print names and weights

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 let pokemonList = [
+    { name: 'Mimikyu', types: ['ghost', 'fairy'], number: '#0778', weight: 1.5},
     { name: 'Gardevoir', types: ['psychic', 'fairy'], number: '#0282', weight: 106.7},
     { name: 'Polteageist', types: ['ghost'], number: '#08550', weight: 0.9},
 ];
 
 
+for (let i = 0; i < pokemonList.length; i++){
+    let pokename = pokemonList[i].name;
+    let pokeweight = pokemonList[i].weight;
+    document.write(`${pokename} (weight: ${pokeweight})<br>`)
+
+
+}
