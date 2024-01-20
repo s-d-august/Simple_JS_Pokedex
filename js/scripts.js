f73ca1084a1bc6d57739e2aa470a0cc7f4a068ed
s-d-august <sinclairaugust@gmail.com>
2024-01-20 16:46:32 -0600

    wrapped array in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
-let pokemonList = [
-    { name: 'Mimikyu', types: ['ghost', 'fairy'], number: '#0778', weight: 1.5},
-    { name: 'Gardevoir', types: ['psychic', 'fairy'], number: '#0282', weight: 106.7},
-    { name: 'Polteageist', types: ['ghost'], number: '#08550', weight: 0.9},
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+          { name: 'Mimikyu', types: ['ghost', 'fairy'], number: '#0778', weight: 1.5},
+          { name: 'Gardevoir', types: ['psychic', 'fairy'], number: '#0282', weight: 106.7},
+          { name: 'Polteageist', types: ['ghost'], number: '#08550', weight: 0.9},
+      ];
+  
+    function add(poke) {
+      pokemonList.push(poke);
+    }
+  
+    function getAll() {
+      return pokemonList;
+    }
+  
+    return {
+      add: add,
+      getAll: getAll
+    };
+  })();
 
 // Lists names and weights of pokemonList
 
