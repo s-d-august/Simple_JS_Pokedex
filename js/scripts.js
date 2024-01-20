6c2479c989ec87076d593b9435afbac26b42fdcd
s-d-august <sinclairaugust@gmail.com>
2024-01-20 17:48:53 -0600

    adjusted code formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,13 @@
 let pokemonRepository = (function () {
     let pokemonList = [
-          { name: 'Mimikyu', types: ['ghost', 'fairy'], number: '#0778', weight: 1.5},
-          { name: 'Gardevoir', types: ['psychic', 'fairy'], number: '#0282', weight: 106.7},
-          { name: 'Polteageist', types: ['ghost'], number: '#08550', weight: 0.9},
+          {name: 'Mimikyu', types: ['ghost', 'fairy'], number: '#0778', weight: 1.5},
+          {name: 'Gardevoir', types: ['psychic', 'fairy'], number: '#0282', weight: 106.7},
+          {name: 'Polteageist', types: ['ghost'], number: '#08550', weight: 0.9},
       ];
   
     function add(poke) {
-      if ((typeof poke) !== Object) {
-        document.write(`This Pokemon isn't formatted correctly!`)
+      if ((typeof poke) !== "object") {
+        document.write(`This Pokemon isn't formatted as an object!<br>`)
     }
     else if ((poke.hasOwnProperty('name' || 'types' || 'number' || 'weight'))) {
         {pokemonList.push(poke);}
@@ -35,5 +35,6 @@ let pokemonRepository = (function () {
         document.write(` Wow, that's big!<br>`);}
         else {document.write('<br>')}
   });
+
 
 
