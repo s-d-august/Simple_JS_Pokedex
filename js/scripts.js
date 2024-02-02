6b18d7696516132b0ad40e306c8fa5ef4037d033
s-d-august <sinclairaugust@gmail.com>
2024-02-02 11:56:13 -0600

    added loadList function

--- js/scripts.js
+++ js/scripts.js
@@ -43,12 +43,30 @@ let pokemonRepository = (function () {
         
     
 
+  function loadList() {
+    return fetch(apiUrl).then(function (response) {
+      return response.json();
+    }).then(function (json) {
+      json.results.forEach(function (item) {
+        let pokemon = {
+          name: item.name,
+          detailsUrl: item.url
+        };
+        add(pokemon);
+      });
+    }).catch(function (e) {
+      console.error(e);
+    })
+  }
+    
+
     return {
         add: add,
         getAll: getAll,
         nameFilter: nameFilter,
         addListItem: addListItem,
         showDetails: showDetails,
+        loadList: loadList,
     };
 })();
 
